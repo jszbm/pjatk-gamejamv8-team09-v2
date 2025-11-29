fd75a69ae1cb78bdf5580ca8e881f7b4d0ea0e6f
Vlad Tavrel <vladislav.tavrel@gmail.com>
2025-11-29 23:43:45 +0100

    Move all my changes

---
+++ Assets/Scripts/Controllers/PauseManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Assets.Scripts.Controllers
+{
+    public class PauseManager : MonoBehaviour
+    {
+        public static bool IsPaused { get; private set; }
+        private void Start()
+        {
+            InputHandler.Instance.InputActions.Menu.Pause.performed += HandlePauseEvent;
+        }
+
+        private void HandlePauseEvent(InputAction.CallbackContext context)
+        {
+            if (context.performed)
+            {
+                IsPaused = !IsPaused;
+                SignalBus.Instance.OnPauseStateChanged.Invoke(IsPaused);
+
+                Time.timeScale = IsPaused ? 0f : 1f;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,32 @@
+using Assets.Scripts.Controllers;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    [SerializeField] private float x_speed = 10;
+    [SerializeField] private float base_y_speed = 3;
+    [SerializeField] private float additional_y_speed = 5;
+    private InputAction speedUpAction;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        speedUpAction = InputHandler.Instance.InputActions.Movement.SpeedUp;
+    }
+   
+    private void FixedUpdate()
+    {
+        var moveDirection = InputHandler.Instance.InputActions.Movement.Direction.ReadValue<float>();
+
+        float final_y_speed = base_y_speed;
+
+        if (speedUpAction.IsPressed())
+        {
+            final_y_speed += additional_y_speed;
+        }
+        
+        rb.linearVelocity = new Vector2(moveDirection * x_speed, -final_y_speed);
+    }
+}
